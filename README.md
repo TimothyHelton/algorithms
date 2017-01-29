8acf99ce2ae51701bd26b39e4fdf8aeddebbea40
Timothy Helton <timothy.j.helton@gmail.com>
2017-01-28 21:48:05 -0700

    FEAT: add Big O notation module

---
+++ README.md
@@ -0,0 +1,9 @@
+# Algorithms Module
+
+## Big O Notation
+
+![alt text](big_o_models.png)
+
+## Search Algorithms
+
+### Binary Search
---
+++ algorithms/big_o.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Big O Notation Module
+
+.. moduleauthor:: Timothy Helton <timothy.j.helton@gmail.com>
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.misc as sci_msc
+
+
+n = np.linspace(0.001, 4, 1000)
+log_n = np.log(n)
+n_log_n = n * log_n
+n_squared = np.square(n)
+n_factorial = sci_msc.factorial(n)
+
+big_o = pd.DataFrame(np.c_[n, log_n, n_log_n, n_squared, n_factorial],
+                     columns=['O(n)', 'O(log n)', 'O(n log n)',
+                              'O(n^2)', 'O(n!)'])
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots(figsize=(10, 8), facecolor='white')
+    fig.canvas.set_window_title('Big O Notation Common Performance Models')
+    big_o.plot(ax=ax, kind='line', style=['--', '-', '-.', ':', '-'])
+    for n in range(5):
+        ax.lines[n].set_linewidth(2)
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticks([])
+    plt.title('Big O Notation Common Performance Models', fontsize=24)
+    plt.xlabel('n', fontsize=18)
+    plt.grid()
+
+    plt.show()
+    # plt.savefig('big_o_models.png')
